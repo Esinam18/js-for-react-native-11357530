c9b44a6a00f16c3ebb1f1341c91de67419e37d71
Esinam18 <148632742+Esinam18@users.noreply.github.com>
2024-05-19 22:04:04 +0000

    Task 2 completed

--- arrayManipulation.js
+++ arrayManipulation.js
@@ -2,7 +2,26 @@ function processArray(numbers) {
     return numbers.map(num => (num % 2 === 0 ? num * num : num * 3));
 }
 
-//example
+//example for task 1
 const numbers = [1, 2, 3, 4, 5];
 const processedNumbers = processArray(numbers);
 console.log(processedNumbers);
+
+function formatArrayStrings(strings, numbers) {
+    if (strings.length !== numbers.length) {
+        throw new Error("Arrays must be of the same length");
+    }
+
+    return strings.map((str, index) => 
+        (numbers[index] % 2 === 0 ? str.toUpperCase() : str.toLowerCase())
+    );
+}
+
+//example for task 2
+const strings = ["apple", "banana", "cherry"];
+const numbers = [1, 2, 3];
+
+const formattedStrings = formatArrayStrings(strings, numbers);
+console.log(formattedStrings);
+
+module.exports = { processArray, formatArrayStrings };
